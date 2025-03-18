58683cb735af633bbc50b52aaa83a78576402cdb
da-nn-yy <db7394928@gmail.com>
2025-03-18 14:16:35 +0300

    add react-scroll dependency for smooth scrolling functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "@/app/Sections/Header";
+import Hero from "@/app/Sections/Hero";
 
 const Home = () => {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
   );
 };
