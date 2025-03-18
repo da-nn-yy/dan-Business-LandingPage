323686d8643d3a7504a10a61a61edd3284f72b84
da-nn-yy <db7394928@gmail.com>
2025-03-18 23:59:41 +0300

    add features data structure for feature list display

---
+++ app/constants/index.js
@@ -0,0 +1,24 @@
+export const features = [
+  {
+    id: "0",
+    icon: "/images/features-1.png",
+    caption: "Demo Caption",
+    title: "FEATURE 1",
+    text: "text-1",
+    button: {
+      icon: "/images/magictouch.svg",
+      title: "watch the demo",
+    },
+  },
+  {
+    id: "1",
+    icon: "/images/features-2.png",
+    caption: "DEMO Caption",
+    title: "FEATURE 2",
+    text: "text-2",
+    button: {
+      icon: "/images/magictouch.svg",
+      title: "watch the demo",
+    },
+  },
+];
