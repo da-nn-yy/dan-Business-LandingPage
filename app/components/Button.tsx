ec594d1f4adb97ecab6a729d2876bed7643e5a16
da-nn-yy <db7394928@gmail.com>
2025-03-18 23:42:26 +0300

    enhance Button component with marker support and improved structure

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,6 +1,37 @@
 import clsx from "clsx";
+import Image from "next/image";
+import Marker from "@/app/components/Marker";
 
-const Button = ({ icon, children, href, containerClassName, onClick }) => {
+const Button = ({
+  icon,
+  children,
+  href,
+  containerClassName,
+  onClick,
+  markerFill,
+}) => {
+  const Inner = () => (
+    <>
+      <span className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
+        <span className="absolute -left-[1px]">
+          <Marker markerFill={markerFill} />
+        </span>
+        {icon && (
+          <Image
+            src={icon}
+            alt="circle"
+            className="size-10 mr-5 object-contain z-10"
+            width={200}
+            height={200}
+          />
+        )}
+        <span className="relative z-2 font-poppins base-bold text-p1 uppercase">
+          {children}
+        </span>
+      </span>
+      <span className="glow-before glow-after"></span>
+    </>
+  );
   return href ? (
     <a
       className={clsx(
@@ -9,14 +40,17 @@ const Button = ({ icon, children, href, containerClassName, onClick }) => {
       )}
       href={href}
     >
-      TEXT
+      <Inner />
     </a>
   ) : (
     <button
-      className={clsx("relative p-0.5 g5 rounded-2xl shadow-500 group")}
+      className={clsx(
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
+        containerClassName,
+      )}
       onClick={onClick}
     >
-      TEXT
+      <Inner />
     </button>
   );
 };
