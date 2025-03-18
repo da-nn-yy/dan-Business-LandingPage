b58c5d42ffe06eb3908c8bf125ebc4b729a7825a
da-nn-yy <db7394928@gmail.com>
2025-03-18 23:59:45 +0300

    add features data structure for feature list display

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "@/app/Sections/Header";
 import Hero from "@/app/Sections/Hero";
+import Features from "@/app/Sections/Features";
 
 const Home = () => {
   return (
     <main className="overflow-hidden">
       <Header />
       <Hero />
+      <Features />
     </main>
   );
 };
