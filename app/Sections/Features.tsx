80bfbe910061da2fbb416edda769c942b75f8576
da-nn-yy <db7394928@gmail.com>
2025-03-20 00:20:14 +0300

    add: integrate details list in Features.tsx for enhanced feature display

--- app/Sections/Features.tsx
+++ app/Sections/Features.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Element } from "react-scroll";
-import { features } from "@/app/constants";
+import { details, features } from "@/app/constants";
 import Button from "@/app/components/Button";
 
 const Features = () => {
@@ -8,7 +8,7 @@ const Features = () => {
     <section>
       <Element name="features">
         <div className="container">
-          <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:gap-3">
+          <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
             {features.map(({ id, icon, caption, title, text, button }) => (
               <div
                 key={id}
@@ -32,7 +32,21 @@ const Features = () => {
                 <Button icon={button.icon}>{button.title}</Button>
               </div>
             ))}
-            <ul className="relative flex justify-around flex-grow px-[5%] border-2 border-s3 rounded-7xl max-md:hidden"></ul>
+            <ul className="relative flex justify-around flex-grow px-[5%] border-2 border-s3 rounded-7xl max-md:hidden">
+              <div className="absolute bg-s3/20 top-[38%] left-0 right-0 w-full h-[1px] z-10" />
+              {details.map(({ id, icon, title }) => (
+                <li key={id} className="relative pt-16 px-4 pb-14">
+                  <div className="absolute top-0 bottom-0 left-1/2 bg-s3/20 w-[1px] h-full z-10" />
+                  <div className="flex justify-center items-center mx-auto mb-3 border-2 border-s2 rounded-full hover:border-s4 transition-all duration-500 shadow-500 size-20">
+                    <img
+                      className="size-17/20 object-contain z-20"
+                      src={icon}
+                      alt={title}
+                    />
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </Element>
