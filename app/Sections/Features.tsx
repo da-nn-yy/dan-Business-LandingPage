f2018fb973e6990f4bc8c624d1d04b20649f9985
da-nn-yy <db7394928@gmail.com>
2025-03-19 14:59:13 +0300

    add: integrate button component and enhance feature display layout

--- app/Sections/Features.tsx
+++ app/Sections/Features.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { Element } from "react-scroll";
+import { Button, Element } from "react-scroll";
 import { features } from "@/app/constants";
-import Image from "next/image";
 
 const Features = () => {
   return (
@@ -17,15 +16,19 @@ const Features = () => {
                 <div className="w-full flex justify-start items-start">
                   <div className="-ml-3 mb-12 flex items-center justify-center flex-col">
                     <div className="w-0.5 h-16 bg-s3" />
-                    <Image
-                      src={icon}
+                    <img
                       className="size-28 object-contain"
+                      src={icon}
                       alt={title}
-                      width={28}
-                      height={28}
                     />
                   </div>
                 </div>
+                <p className="caption mb-5 max-md:mb-6">{caption}</p>
+                <h2 className="max-w-400 mb-7 h3 text-p4 max-md:mb-6 max-md:h5">
+                  {title}
+                </h2>
+                <p className="mb-11 body-1 max-md:mb-8 max-md:body-3">{text}</p>
+                <Button icon={button.icon}>{button.title}</Button>
               </div>
             ))}
           </div>
