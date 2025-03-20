17f0e5f37756a118ec1cc9c6f225bda82388fbd2
da-nn-yy <db7394928@gmail.com>
2025-03-20 22:29:02 +0300

    update: adjust layout and enhance title display in Features.tsx

--- app/Sections/Features.tsx
+++ app/Sections/Features.tsx
@@ -36,7 +36,7 @@ const Features = () => {
               <div className="absolute bg-s3/20 top-[38%] left-0 right-0 w-full h-[1px] z-10" />
               {details.map(({ id, icon, title }) => (
                 <li key={id} className="relative pt-16 px-4 pb-14">
-                  <div className="absolute top-0 bottom-0 left-1/2 bg-s3/20 w-[1px] h-full z-10" />
+                  <div className="absolute top-8 bottom-0 left-1/2 bg-s3/20 w-[1px] h-full z-10" />
                   <div className="flex justify-center items-center mx-auto mb-3 border-2 border-s2 rounded-full hover:border-s4 transition-all duration-500 shadow-500 size-20">
                     <img
                       className="size-17/20 object-contain z-20"
@@ -44,6 +44,9 @@ const Features = () => {
                       alt={title}
                     />
                   </div>
+                  <h3 className="relative z-2 max-w-36 mx-auto my-0 base-small text-center uppercase">
+                    {title}
+                  </h3>
                 </li>
               ))}
             </ul>
