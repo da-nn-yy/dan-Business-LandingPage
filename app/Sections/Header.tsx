713f43a24a8c8753f154b9eca4997dd6f29a661f
da-nn-yy <db7394928@gmail.com>
2025-03-20 23:11:43 +0300

    update: refine scroll detection threshold and adjust header background opacity in Header.tsx

--- app/Sections/Header.tsx
+++ app/Sections/Header.tsx
@@ -11,9 +11,7 @@ const Header = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 100) {
-        setHasScrolled("true");
-      }
+      setHasScrolled(window.scrollY > 32);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -40,7 +38,7 @@ const Header = () => {
     <header
       className={clsx(
         "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
-        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
+        hasScrolled && "py-2 bg-black-100/60 backdrop-blur-[8px]",
       )}
     >
       <div className="container flex h-14 items-center max-lg:px-5">
