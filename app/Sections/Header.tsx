cdffdece62cd053c22cd0122fae7121a5aae0900
da-nn-yy <db7394928@gmail.com>
2025-03-20 23:03:41 +0300

    update: implement scroll detection to adjust header styling in Header.tsx

--- app/Sections/Header.tsx
+++ app/Sections/Header.tsx
@@ -1,12 +1,27 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 import { Link as LinkScroll } from "react-scroll";
 
 const Header = () => {
+  const [hasScrolled, setHasScrolled] = useState("false");
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setHasScrolled("true");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   const NavLink = ({ title }: { title: string }) => (
     <LinkScroll
       className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1
@@ -22,7 +37,12 @@ const Header = () => {
   );
 
   return (
-    <header className="fixed top-0 left-0 z-50 w-full py-10">
+    <header
+      className={clsx(
+        "fixed top-0 left-0 z-50 w-full py-10 transition-all duration-500 max-lg:py-4",
+        hasScrolled && "py-2 bg-black-100 backdrop-blur-[8px]",
+      )}
+    >
       <div className="container flex h-14 items-center max-lg:px-5">
         <a className="lg:hidden flex-1 cursor-pointer z-2">
           <Image src="/images/xora.svg" alt="logo" width={115} height={115} />
