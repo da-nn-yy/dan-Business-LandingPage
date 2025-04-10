89396509f40a5127fc076d0843f0451012891cdb
da-nn-yy <db7394928@gmail.com>
2025-04-10 13:37:39 +0300

    update: add id attribute to features section for improved accessibility

--- app/Sections/Features.tsx
+++ app/Sections/Features.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 
 const Features = () => {
   return (
-    <section>
+    <section id="features">
       <Element name="features">
         <div className="container">
           <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
