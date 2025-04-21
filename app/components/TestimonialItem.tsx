54816b7f7b0d109e7445067d448a964d7e070e0d
da-nn-yy <db7394928@gmail.com>
2025-04-21 16:33:05 +0300

    update: add TypeScript interface and image rendering for TestimonialItem component

--- app/components/TestimonialItem.tsx
+++ app/components/TestimonialItem.tsx
@@ -1,7 +1,11 @@
-import React from "react";
 import clsx from "clsx";
+import Image from "next/image";
 
-const TestimonialItem = ({ item, containerClassName }) => {
+interface testimonialProps {
+  item: string;
+  containerClassName: string;
+}
+const TestimonialItem = ({ item, containerClassName }: testimonialProps) => {
   return (
     <div
       className={clsx(
@@ -10,6 +14,22 @@ const TestimonialItem = ({ item, containerClassName }) => {
       )}
     >
       <blockquote className="h6 mb-8 text-p4">{item.comment}</blockquote>
+      <div className="flex items-center max-xl:-mr-8">
+        <div className="mr-4 size-20 shrink-0 rounded-half border-2 border-s2  p-1.5">
+          <Image
+            src={item.avatarUrl}
+            alt={item.name}
+            className="size-full object-cover"
+            unoptimized
+            width={100}
+            height={100}
+          />
+        </div>
+        <div>
+          <h4 className="body-2 mb-0.5 text-p1">{item.name}</h4>
+          <p className="small-compact uppercase text-s3">{item.role}</p>
+        </div>
+      </div>
     </div>
   );
 };
