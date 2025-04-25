cae2acb4b51bd7c325b84f3e936f2582e7a9ca89
da-nn-yy <db7394928@gmail.com>
2025-04-26 00:24:18 +0300

    update: integrate social media icons into Footer component

--- app/Sections/Footer.tsx
+++ app/Sections/Footer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import { socials } from "@/app/constants";
 
 const Footer = () => {
   return (
@@ -8,21 +9,28 @@ const Footer = () => {
         <div className="flex w-full max-md:flex-col">
           <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
             <p className="opacity-70">Copyright, Da-nn-yy</p>
-            <div className="flex items-center justify-center sm:ml-auto">
-              <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
-                Privacy Policy
-              </p>
-              <p className="text-p5 transition-all duration-500 hover:text-p1">
-                Terms of Use
-              </p>
-            </div>
+          </div>
+          <div className="flex items-center justify-center sm:ml-auto">
+            <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
+              Privacy Policy
+            </p>
+            <p className="text-p5 transition-all duration-500 hover:text-p1">
+              Terms of Use
+            </p>
           </div>
 
-          <ul className="flex flex-1 justify-center gap-3 max-md:mt-10 md:justify-end">
-            {socials.map(({ id url,icon,title}) => (
+          <ul className="flex flex-1 justify-center gap-3 max-md:mt-10">
+            {socials.map(({ id, url, icon, title }) => (
               <li key={id}>
-                <a href={url } className='social-icon'>
-                  <Image src={} alt={}
+                <a href={url} className="social-icon">
+                  <Image
+                    src={icon}
+                    alt={title}
+                    width={24}
+                    height={24}
+                    unoptimized
+                    className="size-1/3 object-contain"
+                  />
                 </a>
               </li>
             ))}
